f6a17a08cf46658b993521fc6f9d93869f10379a
chris <nuke82@nate.com>
2017-11-30 12:23:21 +0900

    완성

--- js/87_interaction_2.js
+++ js/87_interaction_2.js
@@ -2,6 +2,29 @@
 
     $(function(){
 
+        $(".btn_ok").on("click", function(){
+
+            var bidx1 = $(".drop_container_dummy1").find(".drop_space").data("bind-box-idx");
+            var bidx2 = $(".drop_container_dummy2").find(".drop_space").data("bind-box-idx");
+            var bidx3 = $(".drop_container_dummy3").find(".drop_space").data("bind-box-idx");
+
+            console.log(bidx1);
+            console.log(bidx2);
+            console.log(bidx3);
+
+            if (typeof(bidx1) != 'undefined' && typeof(bidx2) != 'undefined' && typeof(bidx3) != 'undefined') {
+
+                if (bidx1 == 1 && bidx2 == 2 && bidx3 == 0) {
+                    $(".img_cong").fadeIn();
+                } else {
+                    alert("틀렸습니다.");
+                }
+
+            } else {
+                alert("물체를 모두 넣어주세요");
+            }
+
+        });
 
         //$(".drop_area").rotate(8);
 
